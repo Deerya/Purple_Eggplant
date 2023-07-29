d02f15a1b6d320651c3ae4fa19af5bc03d90f972
deerya <18738857524@163.com>
2023-07-29 10:18:40 +0800

    Promise封装练习， 并使用nodejs运行成功

---
+++ 1.Promise基本使用/Promise封装练习02-fs模块.js
@@ -0,0 +1,19 @@
+// const { resolve } = require("path");
+
+function mineReadFile(path){
+    return new Promise((resolve,reject)=>{
+        require('fs').readFile(path,(err,data)=>{
+            if(err) reject(err);
+            resolve(data);
+        });
+    });
+};
+
+mineReadFile('./resource/content.txt')
+.then(value=>{
+// 输出文件内容
+console.log(value.toString());
+},reason=>{
+// 
+console.log(reason);
+})
